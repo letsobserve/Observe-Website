d4e9965999a191333a0dc93e3c96454dcf485779
letsobserve <101605169+letsobserve@users.noreply.github.com>
2022-05-19 19:18:19 +1000

    Add files via upload

---
+++ Observe-Website/README.md
@@ -0,0 +1,5 @@
+# letsobserve.github.io
+
+This is a website for Observation
+
+Coded soley by Jordan Hewett
---
+++ Observe-Website/js/observescript.js
@@ -0,0 +1,64 @@
+const tags = ["Observation", "Blog", "Media", "Nature", "Food", "Dreams"];
+
+function setThemeFromCookie() {
+	const themeState = document.getElementById("allBody");
+	themeState.className = isThemeSelected() ? "light-mode" : "dark-mode";
+}
+
+function setThemeSwitchState() {
+	document.getElementById("toggleTheme").checked = isThemeSelected();
+}
+
+function isThemeSelected() {
+	return document.cookie.match(/theme=dark/i) != null;
+}
+
+function toggleTheme() {
+	const themeState = document.getElementById("allBody");
+	const currentState = themeState.classList;
+	const newState = themeState.classList == "light-mode" ? "light-mode" : "dark-mode";
+	var btn = document.getElementById("toggleTheme");
+	var sun = document.getElementById("themeImageSun");
+	var moon = document.getElementById("themeImageMoon");
+
+	themeState.className = newState;
+	document.cookie = "theme=" + (newState == "light-mode" ? "dark" : "light");
+
+	if (btn.checked) {
+		moon.style.opacity = "1";
+		sun.style.opacity = "0";
+		document.getElementById("allBody").classList.toggle("dark-mode");
+		document.getElementById("allBody").classList.toggle("light-mode");
+	}
+	else {
+		sun.style.opacity = "1";
+		moon.style.opacity = "0";
+		document.getElementById("allBody").classList.toggle("light-mode");
+		document.getElementById("allBody").classList.toggle("dark-mode");
+	}
+}
+
+(function() {
+	setThemeFromCookie();
+	setThemeSwitchState();
+	document.getElementById("toggleTheme").onchange = toggleTheme();
+}) ();
+
+function mediaModalFn(element) {
+	var modal = document.getElementById("mediaModal");
+	var modalImg = document.getElementById("mediaModalImg");
+	var alttext = document.getElementById("mediaModalP1");
+	modal.style.display = "flex";
+	modalImg.src = element.src;
+	alttext.innerHTML = element.alt;
+}
+
+function closeMediaModal() {
+	var modal = document.getElementById("mediaModal");
+	modal.style.display = "none";
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
